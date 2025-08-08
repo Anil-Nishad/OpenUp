6039ea4f7cd18b1a03fd25b2fb2f92d42fca131f
ANIL KUMAR <anilnishad298@gmail.com>
2025-08-08 16:02:09 +0530

    Replace hardcoded TimeLine Posts and Data. And Added Partial View

--- OpenUp/Controllers/HomeController.cs
+++ OpenUp/Controllers/HomeController.cs
@@ -1,20 +1,27 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OpenUpData;
 
 namespace OpenUp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly OpenUpContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, OpenUpContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var allPosts = await _context.Posts
+                        .Include(n=>n.User)
+                        .ToListAsync();
+            return View(allPosts);
         }
 
         public IActionResult Privacy()
