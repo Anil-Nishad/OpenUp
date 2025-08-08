384719b3bc96a5e6720e3a5b5e064a31d9555730
ANIL KUMAR <anilnishad298@gmail.com>
2025-08-08 17:53:41 +0530

    Created First text-Only Status from App.

--- OpenUp/Controllers/HomeController.cs
+++ OpenUp/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OpenUp.ViewModels.Home;
 using OpenUpData;
+using OpenUpData.Models;
 
 namespace OpenUp.Controllers
 {
@@ -19,9 +21,31 @@ namespace OpenUp.Controllers
         public async Task<IActionResult> Index()
         {
             var allPosts = await _context.Posts
-                        .Include(n=>n.User)
+                        .Include(n => n.User)
+                        .OrderByDescending(n => n.DateCreated)
                         .ToListAsync();
             return View(allPosts);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost(PostVM post)
+        {
+            //Get the Logged in User
+            int loggedInUser = 1;
+
+            //Create a new Post
+            var newPost = new Post
+            {
+                Content = post.Content,
+                DateCreated = DateTime.Now,
+                DateUpdated = DateTime.Now,
+                UserId = loggedInUser
+            };
+            await _context.Posts.AddAsync(newPost);
+            await _context.SaveChangesAsync();
+
+            //Redirect to the Index Page
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
