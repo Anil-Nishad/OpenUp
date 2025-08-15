253d6b5c8c0f5888b315bb490fdc1917c384ec8a
ANIL KUMAR <anilnishad298@gmail.com>
2025-08-15 23:25:35 +0530

    Implementing Login with Validation

--- OpenUp/Controllers/AuthenticationController.cs
+++ OpenUp/Controllers/AuthenticationController.cs
@@ -21,6 +21,20 @@ public class AuthenticationController : Controller
         return View();
     }
 
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginVM loginVM)
+    {
+        if (!ModelState.IsValid)
+            return View(loginVM);
+
+        var result = await _signInManager.PasswordSignInAsync(loginVM.Email, loginVM.Password, false, false);
+        if (result.Succeeded)
+            return RedirectToAction("Index", "Home");
+
+        ModelState.AddModelError("", "Invalid login attempt");
+        return View(loginVM);
+    }
+
     public async Task<IActionResult> Register()
     {
         return View();
